4e2acbe1d07916a783458083aacf936c5c654f2a
Timmy Truong <timmy.truong@canada.ca>
2019-04-09 15:52:25 -0400

    yes

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const Sequelize = require("sequelize");
 
 
 // setting up the connection to the postgres database
-const db = new Sequelize("scoopdb", "postgres", "123456", {
+const db = new Sequelize("scoopDB", "postgres", "123456", {
   host: "localhost",
-  port: "5432",
+  port: "5434",
   dialect: "postgres",
   operatorAliases: false,
   define: {
